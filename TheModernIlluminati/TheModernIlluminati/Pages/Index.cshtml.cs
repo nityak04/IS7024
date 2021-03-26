4aa628dd8c03a8148ba1da2271d78951c756f32c
nityak04 <77544600+nityak04@users.noreply.github.com>
2021-03-26 02:17:28 -0400

    First api consumed

--- TheModernIlluminati/TheModernIlluminati/Pages/Index.cshtml.cs
+++ TheModernIlluminati/TheModernIlluminati/Pages/Index.cshtml.cs
@@ -1,10 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using QuickType;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+
 
 namespace TheModernIlluminati.Pages
 {
@@ -19,7 +21,18 @@ namespace TheModernIlluminati.Pages
 
         public void OnGet()
         {
-
+            using (var webClient = new System.Net.WebClient())
+            {
+                string jsonString = webClient.DownloadString("http://api.nobelprize.org/v1/country.json");
+                Country country = Country.FromJson(jsonString);
+                List<Count> random1 = country.Countries;
+                List<Count> random2 = new List<Count>();
+                foreach (Count coun in random1)
+                {
+                    random2.Add(coun);
+                }
+                ViewData["Countries"] = random2;
+            }
         }
     }
 }
