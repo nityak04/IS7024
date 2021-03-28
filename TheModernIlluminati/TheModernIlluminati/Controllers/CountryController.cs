d56c0edc1ac1d192c5be67a4f8df55f2a0b24c38
Vibhash Dhar <dharvo@mail.uc.edu>
2021-03-28 02:34:06 -0400

    Layout Changes

--- TheModernIlluminati/TheModernIlluminati/Controllers/CountryController.cs
+++ TheModernIlluminati/TheModernIlluminati/Controllers/CountryController.cs
@@ -10,18 +10,6 @@ namespace TheModernIlluminati.Controllers
     [ApiController]
     public class CountryController : Controller
     {
-       // [Route("country/search/{countrycode}")]
-        //public ActionResult<Country[]> SearchLaureates(string countrycode)
-        //{
-        //    using (var webClient = new WebClient())
-        //    {
-        //        String countryData = webClient.DownloadString("https://data.cityofchicago.org/resource/s6ha-ppgi.json");
-        //        var result= 
 
-        //        houses = AffordableHouse.FromJson(houseJSON);
-        //        housesFiltered = houses.Where(x => x.ZipCode == zipCode).ToArray();
-        //        return housesFiltered;
-        //    }
-        //}
     }
 }
